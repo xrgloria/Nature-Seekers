e7465f1db27169de9907bad3a4dcde76747e5da6
Snguyen5 <snguyen5@umbc.edu>
2016-05-02 13:11:10 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!-- 
+Author: Steven Nguyen
+Date last modified: 05/02/2016
+IS 448
+Professor Sampath
+This document will be used to allow users to access our website.
+-->
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<!-- Title of the webpage -->
+		<title>
+			Login - Nature Seekers
+		</title>
+		
+		<!-- CSS styling and javascript linking -->
+		<link rel="stylesheet" href="style/lavish-bootstrap.css"/>
+		<link rel="stylesheet" href="style/steven.css"/>
+		<script type="text/javascript" src="login.js"></script>
+		
+	</head>
+	
+	<body id="body">
+	
+	<!-- Navigation bar -->
+	<div class="navbar navbar-inverse" style="border-radius:0pt;">
+		<div class="content">
+			<span class="navbar-brand">
+				Nature Seekers
+			</span>
+			<ul class="nav navbar-nav">
+				<li class = "active"><a href="#">Home</a></li>
+				<!-- If statement, if cookies are found, displays the rest of the navbar -->
+				<?php
+				if(isset($_COOKIE['LoggedIn'])){
+					?>
+				<li><a href="./points_of_interest.html ">Add Markers</a></li>
+				<li ><a href="#">View Route</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+		  <li><a href="./logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+		</ul>
+		<!-- End if statement -->
+		<?php
+				}
+				?>
+		</div>
+	</div>
+		
+		<!-- First header of the webpage -->
+		<h1 class="centerhead">
+			<!-- If statement, if cookies are found the user is greeted -->
+			<b>Welcome to Nature Seekers<?php
+				if(isset($_COOKIE['LoggedIn'])){
+				echo ", ".$_COOKIE['LoggedIn'];}
+			?>
+			</b>
+		</h1>
+		<!-- Login form, if cookies are found the form is hidden -->
+		<?php
+			if(!isset($_COOKIE['LoggedIn'])){
+		?>
+		<form action="login2.php" method="POST">
+		<p class="login">
+			Username: <input type="text" id="user" name="Username" size="25"/><br /><br />
+			Password: <input type="password" id="pass" name="Password" size="25"/><br /><br />
+			<form action="identify.php">
+				<input type="submit" id="loginSubmit" value="Login" onclick="validateLogin()" />
+			</form>
+		</p>
+		
+		<!-- Links to assist problems logging in, if cookies are found the buttons are hidden -->
+		<p class="login">
+			<input type="button" id="forgotButton" value="Forgot your Username?" onclick="forgotRedirect()" />
+			<input type="button" id="registerButton" value="Not a member yet?" onclick="registerRedirect()" /><br />
+		</p>
+		
+		</form>
+		<?php
+			}
+		?>
+		<!-- Description of webpage -->
+		<h4 class="centerText">
+			Nature Seekers is a website in which users are able to plot routes, 
+			areas and points of interest on a map and comment on them for assorted 
+			outdoor sports. The information may then be displayed to other users, 
+			creating a user generated map of locations which pertain to specific outdoor 
+			sports and user input on them. This will allow the users to both learn 
+			about new places to enjoy their sport and and share their knowledge with 
+			others about specific locations.<br /><br />
+			
+			<!-- A checkbox with a javascript function that changes the font color, background color, and button colors of this page -->
+			<input type="checkbox" id="chk" value="Night Mode" onchange="nightMode()" /> Night Mode
+		</h4>
+		
+	</body>
+</html>
