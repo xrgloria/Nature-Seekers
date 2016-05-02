f1fa0b6c51d4d40a2cb015bda6ee1bea0bd337f4
Snguyen5 <snguyen5@umbc.edu>
2016-05-02 13:10:56 -0400

    Update and rename login.php to login2.php

--- login2.php
+++ login2.php
@@ -1,9 +1,12 @@
 <?php
-	#Author: Steven Nguyen
-	#Date last modified: 4/14/2016
-	#IS 448
-	#Professor Sampath
-	#This document will check the username and password entered by the user #from login.html and redirect them accordingly.
+
+	/*
+	Author: Steven Nguyen
+	Date last modified: 05/02/2016
+	IS 448
+	Professor Sampath
+	This document will be used to verify the user input from login.php.
+	*/
 	
 	#PHP function that takes the $newURL variable and redirects the user depending on their login result.
 	function Redirect($newURL){
@@ -12,11 +15,20 @@
 	}
 	
 	#connect to mysql database
+	#Natureseeker's database
 	$db = mysql_connect("localhost","root","root");
+	
+	#Steven's database
+	#$db = mysql_connect("studentdb-maria.gl.umbc.edu","snguyen5","natureseekers");
+	
 	if(!$db)
 		exit("Error - could not connect to MySQL");
 	
 	#select database natureSeekers
+	#Steven's database
+	#$er = mysql_select_db("snguyen5");
+	
+	#Natureseeker's database
 	$er = mysql_select_db("natureSeekers");
 	if(!$er)
 		exit("Error - could not select database");
@@ -27,6 +39,10 @@
 	
 	#select * from users where username = '$uname';
 	#Constructs the query to match the login information
+	#Steven's database
+	#$constructed_query = "select * from users where username = '$uname' and password = '$pword'";
+	
+	#Natureseeker's database
 	$constructed_query = "select * from USERS where user_name = '$uname' and password = '$pword'";
 	
 	#Execute query
@@ -45,11 +61,14 @@
 	
 	#If statement that assigns the proper destination to $newURL depending on the login result
 	if($num_rows == 1){
-		$newURL = "search.php";
+		setcookie('LoggedIn', $uname, time()+86400, "/");
+		$newURL = "search.html";
+		
 	}else{
-		$newURL = "login.html";
+		$newURL = "login.php";
 	}
 	
 	#Calls the Redirect function
 	Redirect($newURL);
+	
 ?>
