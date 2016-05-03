d98da1792c78ce6ddb2748225ee05c9f9f39119f
Luke K Schubert <luke5@umbc.edu>
2016-05-03 15:33:16 -0400

    Converting back to goddamn XML

--- getOverlays.php
+++ getOverlays.php
@@ -14,44 +14,44 @@ $latitudeTop = mysql_real_escape_string(htmlspecialchars($_GET['latitudeTop']));
 $longitudeLeft = mysql_real_escape_string(htmlspecialchars($_GET['longitudeLeft']));
 $latitudeBottom = mysql_real_escape_string(htmlspecialchars($_GET['latitudeBottom']));
 $longitudeRight = mysql_real_escape_string(htmlspecialchars($_GET['longitudeRight']));
-$sql = "SELECT O.OVERLAY_ID, O.TYPE, O.ACTIVITY, O.DESCRIPTION
+$sql = "SELECT O.OVERLAY_ID, O.TYPE, O.OVERLAY_ACTIVITY, O.OVERLAY_NAME
 		FROM OVERLAYS O
 		WHERE O.OVERLAY_ID IN (SELECT OVERLAY_ID FROM POINTS WHERE LATITUDE BETWEEN $latitudeBottom AND $latitudeTop 
 			AND LONGITUDE BETWEEN $longitudeLeft AND $longitudeRight)
 		ORDER BY O.OVERLAY_ID;";
 $result = mysql_query($sql);
 
-$jsonData = array();
-$previousOverlayID = -1;
+$xml = new XMLWriter();
+
+$xml->openURI("php://output");
+$xml->startDocument();
+$xml->setIndent(true);
+
+$xml->startElement('OVERLAYS');
 
 if (mysql_num_rows($result) > 0) {
     while($row = mysql_fetch_array($result)) {
-		$id = $row['OVERLAY_ID'];
-		$type = $row['TYPE'];
-		$activity = $row['ACTIVITY'];
-		$description = $row['DESCRIPTION'];
-		$json[$id] = array(
-			'id' => $id,
-			'type' => $type,
-			'activity' => $activity,
-			'description' => $description);
-		$activityResult = mysql_query($activityQuery);
-		$activities = array();
-		$json[$id]['activities'] = $activities;
+		$xml->startElement("OVERLAY");
+		$xml->writeAttribute('OVERLAY_ID', $row['OVERLAY_ID']);
+		$xml->writeAttribute('OVERLAY_TYPE', $row['OVERLAY_TYPE']);
+		$xml->writeAttribute('ACTIVITY_NAME', $row['ACTIVITY_NAME']);
+		$xml->writeAttribute('OVERLAY_NAME', $row['OVERLAY_NAME']);
+		
 		$pointQuery = "SELECT LATITUDE, LONGITUDE FROM POINTS WHERE OVERLAY_ID = $id;";
 		$pointResult = mysql_query($pointQuery);
-		$points = array();
 		while($pointRow = mysql_fetch_array($pointResult)) {
-			array_push($points, array('lat' => $pointRow['LATITUDE'], 'lng' => $pointRow['LONGITUDE']));
+			$xml->startElement("COORDINATES");
+			$xml->startElement("LATITUDE");
+			$xml->writeRaw($pointRow['LATITUDE']);
+			$xml->endElement();
+			$xml->startElement("LONGITUDE");
+			$xml->writeRaw($pointRow['LONGITUDE']);
+			$xml->endElement();
+			$xml->endElement();
 		}
-		if($type = 2){
-			array_push($points, $points[0]);
-		}
-		
-		$json[$id]['points'] = $points;
-			
 	}
 }
-$json = new Services_JSON();
-return $json->encode($data);
+$xml->endElement();
+header('Content-type: text/xml');
+$xml->flush();
 ?>
