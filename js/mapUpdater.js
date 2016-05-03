73f5636aa3595b7b0024f7e6c289f9fd08d24564
Luke K Schubert <luke5@umbc.edu>
2016-05-03 18:04:35 -0400

    something

--- js/mapUpdater.js
+++ js/mapUpdater.js
@@ -30,6 +30,48 @@ function updateMap(){
 		onSuccess: redrawMap
 	});
 }
+function addOverlay(overlay){
+	alert('adding overlay');
+	var newOverlay;
+
+	if(overlay['OVERLAY_TYPE'] == "2"){
+		alert("adding polygon");
+		newOverlay = new google.maps.Polygon({
+		paths: overlay['POINTS'],
+		strokeColor: white,
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: red,
+		fillOpacity: 0.7
+		});
+		newOverlay.setMap(map);
+	} else if(overlay['OVERLAY_TYPE'] == "1"){
+		alert("adding line");
+		newOverlay = new google.maps.Polyline({
+			path: overlay['POINTS'],
+			geodesic: true,
+			strokeColor: red,
+			strokeOpacity: 1.0,
+			strokeWeight: 4
+		});
+	} else{
+		alert(overlay['POINTS'][0][0]);
+		alert("adding point");
+		newOverlay = new google.maps.Marker({
+		map: map,
+		position: overlay['POINTS'][0]
+		});
+	}
+	var infowindow = new google.maps.InfoWindow({
+		content: "<b>Description</b> : " + overlay['OVERLAY_NAME'] + "<br /><b>Activity</b> : " + overlay['ACTIVITY_NAME']});
+	newOverlay.addListener('mouseover', function(e) {
+			infowindow.setPosition(overlay['points'][0]);
+			infowindow.open(map);
+		});
+		newOverlay.addListener('mouseout', function() {
+			infowindow.close();
+	});
+}
 
 
 function redrawMap(ajax){
@@ -68,7 +110,7 @@ function addOverlay(overlay){
 		newOverlay = new google.maps.Marker({
 		map: map,
 		position: overlay['POINTS'][0]
-	});
+		});
 	}
 	var infowindow = new google.maps.InfoWindow({
 		content: "<b>Description</b> : " + overlay['OVERLAY_NAME'] + "<br /><b>Activity</b> : " + overlay['ACTIVITY_NAME']});
